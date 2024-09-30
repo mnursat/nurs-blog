6921ca586f3fb43889a2d0ce1c24967f25e83543
mnursat <nursatmakhsot@gmail.com>
2024-10-01 00:05:08 +0500

    Initial commit

---
+++ Web/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        './Views/**/*.cshtml',
+        './Pages/**/*.cshtml',
+        './wwwroot/**/*.html',
+        './**/*.razor',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
